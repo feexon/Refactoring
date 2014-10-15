eec0a25769b9efc5478d36097e8720ab74344e05
L.x <feexon@foxmail.com>
2014-10-15 15:16:48 +0800

    Prepareing some code for ExtractMethod

---
+++ ComposingMethods/ExtractMethod/src/main/java/Order.java
@@ -0,0 +1,14 @@
+/**
+ * Created by L.x on 14-10-15.
+ */
+public class Order {
+    private int amount;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
