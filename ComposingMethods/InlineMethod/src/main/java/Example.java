9fe9056f42575a0573ef9ed18bcf348338a868bb
L.x <feexon@foxmail.com>
2014-10-15 16:17:01 +0800

    InlineMethod

--- ComposingMethods/InlineMethod/src/main/java/Example.java
+++ ComposingMethods/InlineMethod/src/main/java/Example.java
@@ -5,10 +5,7 @@ public class Example {
     private int _numberOfLateDeliveries;
 
     int getRating() {
-        return (moreThanFiveLateDeliveries()) ? 2 : 1;
+        return _numberOfLateDeliveries > 5 ? 2 : 1;
     }
 
-    boolean moreThanFiveLateDeliveries() {
-        return _numberOfLateDeliveries > 5;
-    }
 }
